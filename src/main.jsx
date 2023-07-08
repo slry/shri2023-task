84c999161f3a4b66a45a27c63e0faa788a992240
slry <v.rybalchenko@innopolis.university>
2023-07-08 19:21:52 +0300

    feat: move project on vite

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './reset.css'
+import './fonts.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />
 )
