f4e64ca3bf90a277ae1304ac28609f54aa49f01a
slry <v.rybalchenko@innopolis.university>
2023-07-08 20:14:10 +0300

    fix: fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
-import './reset.css'
 import './fonts.css'
+import './reset.css'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/shri2023-task/'
+  base: '/shri2023-task/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 })
